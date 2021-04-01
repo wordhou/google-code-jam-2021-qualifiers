cb7135e358774a87f5132aa674a63e26b2cfff1e
Edward Hou <36526373+wordhou@users.noreply.github.com>
2021-04-01 13:32:45 -0700

    Update reversort.hs

--- src/reversort.hs
+++ src/reversort.hs
@@ -9,6 +9,7 @@ getParams = getLine >> map read . words <$> getLine :: IO [Int]
 
 solve :: [Int] -> Int
 solve = go 1 where
+  go :: Int -> [Int] -> Int
   go _ [] = 0
   go n xs = let (xs', _:xs'') = span (/= n) xs in
                 1 + length xs' + go (n + 1) (reverse xs' ++ xs'')
